4c17359a981680437e2cfb9aab9678ec323ab617
ECINIntegration <39339631+ECINIntegration@users.noreply.github.com>
2018-06-07 13:48:42 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,43 +15,47 @@
         var pt = patient.read();
         
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+          type: 'Observation',
+          query: {
+            code: {
+              $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                    'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                    'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+            }
+          }
+        });
         
         var im = smart.patient.api.fetchAll({
-                    type: 'Immunization'
-                  });
+          type: 'Immunization'
+        });
         
         var dr = smart.patient.api.fetchAll({
-                    type: 'DiagnosticReport'
-                  });
+          type: 'DiagnosticReport'
+        });
         
         var ma = smart.patient.api.fetchAll({
-                    type: 'MedicationAdministration'
-                  });
+          type: 'MedicationAdministration'
+        });
         
         var mo = smart.patient.api.fetchAll({
-                    type: 'MedicationOrder'
-                  });
+          type: 'MedicationOrder'
+        });
         
         var ms = smart.patient.api.fetchAll({
-                    type: 'MedicationStatement'
-                  });
+          type: 'MedicationStatement'
+        });
 
-        var allObv = smart.patient.api.fetchAll({
+        var allobv = smart.patient.api.fetchAll({
           type: 'Observation'
-          });
-        
-        $.when(pt, obv, im, dr, ma, mo, ms, allObv).fail(onError);
+        });
 
-        $.when(pt, obv, im, dr, ma, mo, ms, allObv).done(function(patient, obv, imm, diagRpt, medicAdmin, medicOrder, medicStmnt, allObv) {
+        var docref = smart.patient.api.fetchAll({
+          type: 'DocumentReference'
+        });
+        
+        $.when(pt, obv, im, dr, ma, mo, ms, allobv, docref).fail(onError);
+
+        $.when(pt, obv, im, dr, ma, mo, ms, allobv, docref).done(function(patient, obv, imm, diagRpt, medicAdmin, medicOrder, medicStmnt, allobv, docref) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
